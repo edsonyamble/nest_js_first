fd4789fa2233efb9fc72fc15d52ffa7f501fe198
Edson Yamble <edsonyamble2@gmail.com>
2024-08-11 07:58:49 +0700

    add graphql

--- src/flowers-graphql/flowers-graphql.module.ts
+++ src/flowers-graphql/flowers-graphql.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { FlowersGraphqlService } from './flowers-graphql.service';
 import { FlowersGraphqlResolver } from './flowers-graphql.resolver';
 import { FlowersService } from 'src/flowers/flowers.service';
 import { PrismaService } from 'src/flowers/prisma.service';
--- src/flowers-graphql/flowers-graphql.resolver.ts
+++ src/flowers-graphql/flowers-graphql.resolver.ts
@@ -1,10 +1,11 @@
 import { Query, Resolver } from '@nestjs/graphql';
-import { FlowersGraphqlService } from './flowers-graphql.service';
 import { FlowersService } from 'src/flowers/flowers.service';
+import { FlowerModel } from './flower.model'
+
 
 @Resolver()
 export class FlowersGraphqlResolver {
-  constructor(private readonly flowersService: FlowersService) {}
-  @Query(() => String,{name:'flowers'})
-  finfAll(){}
+  constructor(private readonly flowersService: FlowersService) { }
+  @Query(() => [FlowerModel], { name: 'flowers' })
+  finfAll() { return this.flowersService.findAll() }
 }
