72aca84bbaadc871f9e6badbc318e4ad1ac4bae9
Edson Yamble <edsonyamble2@gmail.com>
2024-08-13 07:55:10 +0700

    test unit

--- src/flowers/flowers.controller.spec.ts
+++ src/flowers/flowers.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FlowersController } from './flowers.controller';
 import { FlowersService } from './flowers.service';
+import { create } from 'domain';
 
 describe('FlowersController', () => {
   let controller: FlowersController;
@@ -8,13 +9,58 @@ describe('FlowersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FlowersController],
-      providers: [FlowersService],
+      providers: [
+        {
+          provide: 'FlowersService',
+          useValue: {
+            findALL: jest.fn().mockResolvedValue([
+              {
+                id: 1,
+                name: 'Rose',
+                color: 'Red',
+                price: 10,
+              },
+            ]),
+            create: jest.fn().mockResolvedValue({
+              id: 2,
+              name: 'Lily',
+              color: 'White',
+              price: 15,
+            }),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<FlowersController>(FlowersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should return an array of flowers ', async () => {
+    expect(await controller.findAll()).toEqual([
+      {
+        id: 1,
+        name: 'Rose',
+        color: 'Red',
+        price: 10,
+      },
+    ]);
+  });
+
+  it('should return an array of flowers ', async () => {
+    expect(
+      await controller.create({
+        name: 'Lily',
+        color: 'White',
+        price: 15,
+        updateAt: null,
+      }),
+    ).toEqual([
+      {
+        id: 2,
+        name: 'Lily',
+        color: 'White',
+        price: 15,
+      },
+    ]);
   });
 });
--- src/flowers/flowers.service.spec.ts
+++ src/flowers/flowers.service.spec.ts
@@ -1,18 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FlowersService } from './flowers.service';
+import { PrismaService } from './prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('FlowersService', () => {
   let service: FlowersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FlowersService],
+      providers: [
+        FlowersService,
+        {
+          provide: 'PrismaService',
+          useValue: {
+            flower: {
+              findMany: jest.fn().mockResolvedValue([
+                {
+                  id: 1,
+                  name: 'Rose',
+                  color: 'Red',
+                  price: 10,
+                },
+              ]),
+              create: jest.fn().mockResolvedValue({
+                id: 2,
+                name: 'Lily',
+                color: 'White',
+                price: 15,
+              }),
+            },
+          },
+        },
+        { provide: ConfigService, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<FlowersService>(FlowersService);
   });
+    it('should return an array of flowers ', async () => {
+      expect(await service.findAll()).toEqual([
+        {
+          id: 1,
+          name: 'Rose',
+          color: 'Red',
+          price: 10,
+        },
+      ]);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should return an array of flowers ', async () => {
+      expect(
+        await service.create({
+          name: 'Lily',
+          color: 'White',
+          price: 15,
+          updateAt: null,
+        }),
+      ).toEqual([
+        {
+          id: 2,
+          name: 'Lily',
+          color: 'White',
+          price: 15,
+        },
+      ]);
+    });
 });
